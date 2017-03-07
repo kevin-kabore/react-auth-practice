44519f820708168c2612bcf73d447b086687a7db
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-03-06 17:30:09 -0800

    Completes app and router set up

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express()
+const router = require('./router')
 
 // App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3000
-const server = http.createServer(app)
-server.listen(port)
+const server = http.createServer(app);
+server.listen(port);
 console.log('Server listening on port ', port);
