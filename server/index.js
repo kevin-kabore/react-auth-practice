a39fcf5029d8733cf80a31ef876e4352bcb0f068
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-03-13 21:49:23 -0700

    creates SignUp Controller and sets up DB

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const app = express()
-const router = require('./router')
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined'));
